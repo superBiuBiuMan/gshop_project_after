27a5143b4647453bc74cd1c4342f2942347c0f9b
superBiuBiu <1464815392@qq.com>
2022-05-04 18:58:35 +0800

    清理不需要的文件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">登 录</h3>
       </div>
 
       <el-form-item prop="username">
@@ -141,6 +141,8 @@ $cursor: #fff;
 
 /* reset element-ui css */
 .login-container {
+  background-image: url(../../assets/bg.jpg);
+  background-size: cover;
   .el-input {
     display: inline-block;
     height: 47px;
