43f4e3344eb0d3b07e25d38aaaa77f58283b7271
superBiuBiu <1464815392@qq.com>
2022-05-06 16:06:57 +0800

    表单规则添加

--- src/views/product/trademark/List.vue
+++ src/views/product/trademark/List.vue
@@ -57,14 +57,13 @@
     <!-- 添加品牌是不需要id的,而修改品牌是需要id的 -->
     <el-dialog :title="tmForm.id?'修改品牌':'添加品牌'" :visible.sync="dialogFormVisible">
       <!-- el-form通过:model="form" form为一个对象,用于将表单所有数据进行收集 -->
-      <el-form :model="tmForm" style="width:80%">
+      <el-form :model="tmForm" :rules="rules" style="width:80%" ref="tmForm">
         <!-- 品牌名称 -->
-        <el-form-item label="品牌名称" label-width="100px">
+        <el-form-item label="品牌名称" label-width="100px" prop="tmName">
           <el-input v-model="tmForm.tmName" autocomplete="off"></el-input>
         </el-form-item>
-
         <!-- 上传文件  -->
-        <el-form-item label="品牌LOGO" label-width="100px">
+        <el-form-item label="品牌LOGO" label-width="100px" prop="logoUrl">
             <el-upload
                 class="avatar-uploader"
                 action="/dev-api/admin/product/fileUpload"
@@ -86,10 +85,20 @@
 </template>
 
 <script>
-import { trademark } from '@/api';
 export default {
   name: "Trademark",
   data() {
+    // 自定义校验规则
+    var checkTmName = (rule,value,callback)=>{
+      //rule不用管,占位符
+      //value为验证的值,触发器触发一次就传入一次
+      //callback()代表通过 callback(new Error("错误信息"))代表不通过
+      if(value.length < 2 || value.length > 10){
+        callback(new Error("请输入2-10位长度的名称!"));
+      }else{
+        callback();
+      }
+    }
     return {
       //当前页
       page: 1,
@@ -101,18 +110,38 @@ export default {
       trademarkList: [],
       //控制对话框的显示隐藏
       dialogFormVisible:false,
+      //收集表单数据
       tmForm:{
         //品牌名称
         tmName:"",
         //logo真实地址
         logoUrl:""
       },
+      //验证规则(由对象组成)
+        //key是验证规则名称,value是数组,数组里面是对象,一个对象就是一个验证
+        //触发时机有三种情况,失去焦点的时候,输入框改变的时候,整体校验的时候
+      rules:{
+        // 品牌名称校验
+        tmName:[
+          // trigger:'blur' 代表失去焦点
+          // {required:true,message:"请输入品牌名称",trigger:'blur'},
+          // trigger:'change' 代表值发生改变
+          // {min:2,max:10,message:"请输入2~10位数组的品牌",trigger:'change'}
+          //自定义校验规则
+          {validator:checkTmName,trigger:'change'}
+        ],
+        // logoUrl校验
+        logoUrl:[
+          {required:true,message:"请上传图片!",trigger:'change'}
+        ]
+      }
     };
   },
   mounted() {
     this.getBaseTradeMark();
   },
   methods: {
+    
     // 用户单击删除
     removeDialog(info){
       this.$confirm(`确认删除${info.tmName}吗?`, '提示', {
@@ -138,29 +167,35 @@ export default {
         });          
       });
     },
-
     // 用户单击确认添加/修改
-    async confirmAddOrEdit(){
-      //1.收集用户信息
-      let tradeMarkInfo = this.tmForm;
-      //2. 整理收集的信息
-      // 这里不需要整理
-      //3.发送请求
-      try{
-        //4.成功干什么
-        await this.$API.trademark.addOrChange(tradeMarkInfo);
-        // 关闭弹窗
-        this.dialogFormVisible = false;
-        // 更新页码
-          //有id,就继续在这一页,否则就跳到第一页
-        this.getBaseTradeMark(tradeMarkInfo.id?this.page:1);
-        //成功消息提示
-        this.$message.success(tradeMarkInfo.id?"修改信息成功!":"添加品牌成功!");
-
-      }catch(error){
-        //5.失败干什么
-        this.$message.error(tradeMarkInfo.id?"修改信息失败!":"添加品牌失败!")
-      }
+    confirmAddOrEdit(){
+      this.$refs.tmForm.validate(async (isPass)=>{
+        if(isPass){
+          //通过
+          //1.收集用户信息
+          let tradeMarkInfo = this.tmForm;
+          //2. 整理收集的信息
+          // 这里不需要整理
+          //3.发送请求
+          //4.成功干什么
+          try{
+            await this.$API.trademark.addOrChange(tradeMarkInfo);
+            // 关闭弹窗
+            this.dialogFormVisible = false;
+            // 更新页码
+              //有id,就继续在这一页,否则就跳到第一页
+            this.getBaseTradeMark(tradeMarkInfo.id?this.page:1);
+            //成功消息提示
+            this.$message.success(tradeMarkInfo.id?"修改信息成功!":"添加品牌成功!");
+          }catch(error){
+             //5.失败干什么
+            this.$message.error(tradeMarkInfo.id?"修改信息失败!":"添加品牌失败!")
+          }
+        }else{
+          //未通过表单校验
+          this.$message.warning("请输入完整的信息!");
+        }
+      })
     },
     // 修改品牌对话框显示
     showEditDialog(row){
